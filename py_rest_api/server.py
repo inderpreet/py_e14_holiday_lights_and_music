d70e46f143e0a7dac521da8c2e198af74bfa4413
inderpreet <er.inderpreet@gmail.com>
2019-01-11 21:26:36 -0500

    Working

--- py_rest_api/server.py
+++ py_rest_api/server.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS, cross_origin
+from blelights.ble_lights import *
 
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 api = Api(app)
 cors = CORS(app)
+
+v = pack("I", 0x00000000)
 
 class Lights(Resource):
     """ 
@@ -18,6 +21,10 @@ class Lights(Resource):
         g = rgb[2:4]
         b = rgb[4:6]
         str = "Light selected was {} r:{} g:{} b:{} ".format(id,r,g,b)
+        rgb =  r + g + b + '00'
+        val = rgb.decode('hex')
+        id_ = int(id)
+        writeLight(id_, val)
         print (str)
         return str
 
@@ -37,4 +44,7 @@ def color():
 
 if __name__ == '__main__':
     ## Run Flask App
+    v = pack("I", 0x00000000)
+    writeLight(1,v)
+    writeLight(2,v)
     app.run(host='0.0.0.0')
